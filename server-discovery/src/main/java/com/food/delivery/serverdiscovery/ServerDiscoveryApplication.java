6e239408db7117a4fc31946d9d01e66b3c98483b
Arkadiusz <szastarek@gmail.com>
2020-03-17 18:46:05 +0100

    Initial commit

---
+++ server-discovery/src/main/java/com/food/delivery/serverdiscovery/ServerDiscoveryApplication.java
@@ -0,0 +1,17 @@
+package com.food.delivery.serverdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableEurekaClient
+public class ServerDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerDiscoveryApplication.class, args);
+	}
+
+}
